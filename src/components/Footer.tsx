799599f55d5b8147ba040c124c8042e76288822f
mennelsari <mennel27@yahoo.fr>
2024-02-06 15:44:53 +0100

    added reviews and axios for test

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,19 @@
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div>
+      <footer className="footer footer-center p-10 bg-[#60BFB2] text-base-content rounded">
+  <nav className="grid grid-flow-col gap-4">
+    <a className="link text-white link-hover">Contact</a>
+    <a className="link text-white link-hover">Qui sommes-nous</a>
+    <a className="link text-white link-hover">Mentions légales</a>
+  </nav> 
+  
+  <aside>
+    <p className="text-white">Copyright © 2024 - O'bambino </p>
+  </aside>
+</footer>
+    </div>
   )
 }
 
--- src/components/HeaderPublic.tsx
+++ src/components/HeaderPublic.tsx
@@ -1,4 +1,5 @@
 import {Link} from 'react-router-dom';
+import HautGauche from "../assets/hautgauche.png";
 
 const HeaderPublic = () => {
   return (
@@ -10,11 +11,11 @@ const HeaderPublic = () => {
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </div>
       <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
+        <li> <Link to="/">ACCUEIL</Link></li>
         
-        <li><a>Item 3</a></li>
       </ul>
     </div>
+    
     <p className="text-xl">Logo</p>
   </div>
   
