b012ae9b1062e6f30e67d9e9a7544651938762ce
mennelsari <mennel27@yahoo.fr>
2024-02-06 14:50:08 +0100

    test api OK on HeroPublic components

--- src/components/HeroPublic.tsx
+++ src/components/HeroPublic.tsx
@@ -1,17 +1,46 @@
-import React from 'react'
+import axios from "axios";
+import Balloon from '../assets/Balloon.png'
+import { useState, useEffect } from "react";
 
 const HeroPublic = () => {
+
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://maxime-le-goff.vpnuser.lan/apo/projet-03-o-bambino-back/public/api/test');
+        setData(response.data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+  if (!data) return null;
+
   return (
 
-        <div className="hero min-h-screen bg-[#FFE1CC]">
+ <div className="hero min-h-screen bg-[#FFE1CC]">
   <div className="hero-content flex-col lg:flex-row-reverse">
-    <img src="" className="max-w-sm rounded-lg shadow-2xl" />
+    <img src={Balloon} className="max-w-sm rounded-lg" />
     <div>
       <h1 className="text-5xl font-bold">O'bambino</h1>
       <h2>Vos p’tis bouts toujours proches de vous
         </h2>
+        {data.map((item,index) => (
+        <div key={index}>
+        {item.starter} {item.dessert}
+        </div>
+    ))}
       <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-primary">Get Started</button>
     </div>
   </div>
 </div>
