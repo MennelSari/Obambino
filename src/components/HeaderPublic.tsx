916300d73193200516a18c0311da24d675ee59f5
mennelsari <mennel27@yahoo.fr>
2024-02-08 17:34:40 +0100

    changed background color of HeaderPublic

--- src/components/HeaderPublic.tsx
+++ src/components/HeaderPublic.tsx
@@ -2,7 +2,7 @@ import {Link} from 'react-router-dom';
 
 const HeaderPublic = () => {
   return (
-    <div className="navbar bg-transparent">
+    <div className="navbar bg-[#FFE1CC]">
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
