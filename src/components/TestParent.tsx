f6a00a333cbc56cb1647cddfbee39a4bf65e1be0
mennelsari <mennel27@yahoo.fr>
2024-02-12 14:52:22 +0100

    conflicts fixed

--- src/components/TestParent.tsx
+++ src/components/TestParent.tsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import HeaderParent from './HeaderParent'
 const TestParent = () => {
     return (
@@ -10,15 +10,3 @@ const TestParent = () => {
   }
   
   export default TestParent
-=======
-
-const TestParent = () => {
-  return (
-    <div>
-        <h1>CETTE PAGE DOIT ETRE VISIBLE QUE SI PARENT CONNECTÉ</h1>
-        </div>
-  )
-}
-
-export default TestParent
->>>>>>> 7457f47d79d337e99e63efa0e9df68c67c23e463
