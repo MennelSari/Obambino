5a026898aeed6d3025b526e3d5292aab3fb76485
mennelsari <mennel27@yahoo.fr>
2024-02-13 10:33:23 +0100

    navbar for HomePublic is OK, changed the ACCUEIL button

--- src/components/HeaderPublic.tsx
+++ src/components/HeaderPublic.tsx
@@ -11,7 +11,7 @@ const HeaderPublic = () => {
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
           </div>
           {/* Mobile menu */}
-          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-[#FF7B4D] rounded-box w-52">
             <li><Link to="/">ACCUEIL</Link></li>
 
           </ul>
@@ -20,17 +20,14 @@ const HeaderPublic = () => {
           <Link to="/">
             <img src={Logo} alt="" className="hidden md:block w-64 h-64 object-contain object-center" />
           </Link>
+  
         </div>
       </div>
       {/* Desktop menu */}
-      <div className="navbar-end hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li><Link to="/">ACCUEIL</Link></li>
-
-        </ul>
-      </div>
-      <div className="navbar-end">
-        <Link to="/register" className="btn bg-[#FF7B4D]">INSCRIPTION</Link>
+     
+      <div className="navbar-end flex">
+    <Link to="/" className='px-4 hover:px-4 hover:py-4 hover:bg-gray-100 rounded-md hidden lg:block'>ACCUEIL</Link>
+        <Link to="/register" className="btn bg-[#FF7B4D] ml-2">INSCRIPTION</Link>
         <Link to="/login" className="btn bg-[#FF7B4D]">CONNEXION</Link>
       </div>
 
