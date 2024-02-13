408b78aa958ed48b900a4731f851d788f3d2c90b
mennelsari <mennel27@yahoo.fr>
2024-02-13 10:09:49 +0100

    logo added

--- src/components/HeaderPublic.tsx
+++ src/components/HeaderPublic.tsx
@@ -1,59 +1,40 @@
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import Logo from '../assets/logosvg.svg';
+import "../index.css";
 
 const HeaderPublic = () => {
   return (
     <div className="navbar bg-[#FFE1CC]">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+          </div>
+          {/* Mobile menu */}
+          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+            <li><Link to="/">ACCUEIL</Link></li>
+
+          </ul>
+        </div>
+        <div className="navbar max-h-20">
+          <Link to="/">
+            <img src={Logo} alt="" className="hidden md:block w-64 h-64 object-contain object-center" />
+          </Link>
+        </div>
       </div>
-      {/* Mobile menu */}
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><Link to="/">ACCUEIL</Link></li>
-       
-      </ul>
-    </div>
-    <a className="btn btn-ghost text-xl hidden md:block">O'bambino</a>
-  </div>
-  {/* Desktop menu */}
-  <div className="navbar-end hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><Link to="/">ACCUEIL</Link></li>
-      
-    </ul>
-  </div>
-  <div className="navbar-end">
-  <Link to="/register" className="btn bg-[#FF7B4D]">INSCRIPTION</Link>
-  <Link to="/login" className="btn bg-[#FF7B4D]">CONNEXION</Link>
-  </div>
-  {/*<div>
-      <div className="navbar bg-[#FFE1CC]">
-<div className="navbar-start">
-  <div className="dropdown">
-    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-    </div>
-    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-      <li> <Link to="/">ACCUEIL</Link></li>
-      
-    </ul>
-  </div>
-  
-  <p className="text-xl">Logo</p>
-</div>
+      {/* Desktop menu */}
+      <div className="navbar-end hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li><Link to="/">ACCUEIL</Link></li>
 
-<div className="navbar-end">
-<ul className="menu menu-horizontal px-1 font-raleway">
-    <Link to="/">ACCUEIL</Link>
-  </ul>
-  <Link to="/register" className="btn bg-[#FF7B4D]">INSCRIPTION</Link>
-  <Link to="/login" className="btn bg-[#FF7B4D]">CONNEXION</Link>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <Link to="/register" className="btn bg-[#FF7B4D]">INSCRIPTION</Link>
+        <Link to="/login" className="btn bg-[#FF7B4D]">CONNEXION</Link>
+      </div>
 
-</div>
-</div>
-</div>*/}
-</div>
+    </div>
   )
 }
 
